f3c3adf05ad52de1b3d392a6190ffe399618be85
Thomas Geraghty <thethomaseffect@gmail.com>
2020-10-28 15:08:44 +0000

    Switch to modern method of exposing env vars to client

--- next.config.js
+++ next.config.js
@@ -12,11 +12,6 @@ const nextConfig = {
     swSrc: './public/sw.js',
     swDest: './public/service-worker.js',
   },
-  // Exposes Server ENV Vars To Client Using Webpack
-  env: {
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  },
 };
 
 module.exports = withPlugins([[offline]], nextConfig);
