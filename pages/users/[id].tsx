5370bfffc8d0e0a6cb90cc59f045bb47fa781005
Thomas Geraghty <thethomaseffect@gmail.com>
2020-10-29 18:14:04 +0000

    Add image and layout to user page and upgrade some dependencies

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,21 +1,58 @@
-import { Container } from '@material-ui/core';
+import { Container, createStyles, Grid, makeStyles, Paper, Theme, Typography } from '@material-ui/core';
+import Image from 'material-ui-image';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import { getAllUserIds, getUserById } from 'adapters/db/user';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import * as React from 'react';
 import { User } from 'types';
 import { Layout } from '../../components/Layout';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+    },
+    location: {
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
 const UserPage: React.FC<{ user: User }> = ({ user }) => {
+  const classes = useStyles();
+
   return (
     <Layout>
       <Container>
-        Name: {user.surnameFirst ? user.lastName + ' ' + user.firstName : user.firstName + ' ' + user.lastName}
         <br />
-        Location:{' '}
-        {user.currentLocation && 'geo' in user.currentLocation ? 'A complex location' : user.currentLocation?.simple}
-        <br />
-        About: {user.about}
-        <br />
+        <div>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={3}>
+              <Paper className={classes.paper}>
+                <Typography variant="h4">
+                  {user.surnameFirst ? user.lastName + ' ' + user.firstName : user.firstName + ' ' + user.lastName}
+                </Typography>
+                <Typography className={classes.location} variant="h6">
+                  <LocationOnIcon />{' '}
+                  {user.currentLocation && 'geo' in user.currentLocation
+                    ? 'A complex location'
+                    : user.currentLocation?.simple}
+                </Typography>
+                <Image src="http://loremflickr.com/300/200" />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <Paper>
+                <Typography variant="h3">About</Typography>
+                <br />
+                <Typography>{user.about}</Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
       </Container>
     </Layout>
   );
